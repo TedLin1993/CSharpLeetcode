b35e5055486b7365d73f854efa623488c71b5305
TedLin1993 <xup6y3ru04@gmail.com>
2023-10-14 10:11:54 +0800

    add Subsets

---
+++ leetcode/Subsets.cs
@@ -0,0 +1,23 @@
+namespace CSharpLeetcode.leetcode
+{
+    public class Subsets_
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            var res = new List<IList<int>>();
+            res.Add(new List<int>());
+            var cur = new List<int>();
+            void dfs(int idx)
+            {
+                if (idx == nums.Length) return;
+                cur.Add(nums[idx]);
+                res.Add(new List<int>(cur));
+                dfs(idx + 1);
+                cur.RemoveAt(cur.Count - 1);
+                dfs(idx + 1);
+            }
+            dfs(0);
+            return res;
+        }
+    }
+}
