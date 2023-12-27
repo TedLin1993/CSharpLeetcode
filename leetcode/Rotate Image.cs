a8d6644a03281ff7b0b23450a9c5296fd0e7eb39
TedLin1993 <xup6y3ru04@gmail.com>
2023-12-27 11:56:54 +0800

    add Rotate Image

---
+++ leetcode/Rotate Image.cs
@@ -0,0 +1,17 @@
+namespace CSharpLeetcode.leetcode;
+
+public class Solution
+{
+    public void Rotate(int[][] m)
+    {
+        var n = m.Length;
+        for (int i = 0; i < n / 2; i++)
+        {
+            for (int j = 0; j < n - 1 - i * 2; j++)
+            {
+                (m[i][i + j], m[i + j][n - 1 - i], m[n - 1 - i][n - 1 - i - j], m[n - 1 - i - j][i])
+                    = (m[n - 1 - i - j][i], m[i][i + j], m[i + j][n - 1 - i], m[n - 1 - i][n - 1 - i - j]);
+            }
+        }
+    }
+}
